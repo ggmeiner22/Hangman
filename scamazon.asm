83a30f5823066ed7fec60ef7a761eb8fbc15fd90
Garrett Gmeiner <91228641+ggmeiner22@users.noreply.github.com>
2024-04-16 10:18:45 -0400

    Add files via upload
    game2

---
+++ scamazon.asm
@@ -0,0 +1,380 @@
+INCLUDE Irvine32.inc
+includelib winmm.inc
+.data
+wager DWORD ?
+
+directions BYTE "Press any key to read through the lore.", 0
+intro1 BYTE "And so the lore begins...", 0
+intro2 BYTE "After spending the entire day in Dr. Stansifer's office hours,", 0
+intro3 BYTE "Dr. Silaghi finally ran out of money (Who would have though this would happen) (definitly not me)", 0
+intro4 BYTE "Dr. Silaghi was miserable, what was he to do!", 0
+
+intro5 BYTE "Dr. Fitz loves sportscars!", 0
+intro6 BYTE "He just bought his dream car, a dodge charger!", 0
+intro7 BYTE "As he was leaving the dealership he had to stop by his office to pick up some plants.", 0
+
+intro8 BYTE "Dr. Silaghi was in front of pdh.", 0
+intro9 BYTE "He saw Dr. Fitz driving through the parking lot.", 0
+intro10 BYTE "In an act of desperation, he was 'pushed by a strong breeze' into the street!", 0
+intro11 BYTE "Dr. Fitz quicky slamed on the breaks, but it was too late!", 0
+intro12 BYTE "His colleague fell on the hood of his car and rolled onto the winshield before sloping back down", 0
+
+intro13 BYTE "Dr. Fitz was shocked by this, and immediatly got out of the car to check on Dr. Silaghi", 0
+intro14 BYTE "Dr. Silaghi got up fast, but said his 'neck hurt'", 0
+intro15 BYTE "Dr. Fitz took him to his office to talk it over", 0
+
+intro16 BYTE "He apoligized for not seeing him, and offered something that not even Dr. Silaghi could resist!", 0
+intro17 BYTE "He offered him full access to his amazon web services (AWS)", 0
+intro18 BYTE "Now he could host his personal website!", 0
+intro19 BYTE "However, Dr. Fitz had installed malware in his computer through a backdoor!", 0
+intro20 BYTE "Dr. Silaghi was sucked into his computer like Jumanji!", 0
+intro21 BYTE "WELCOME... TO SCAMAZON WEB SERVICES!", 0
+
+intro22 BYTE "Luckily, Dr. Silaghi had a $25 Scamazon gift card in his eWallet.", 0
+intro23 BYTE "Help him reach $150 to afford a new bike!", 0
+
+bal BYTE "TOTAL BALANCE : ", 0
+bet BYTE "How much would you like to wager? : ", 0
+
+invalid1 BYTE "Joe Biden does not like that input", 0
+
+co1 BYTE "Would you like to cash out? Type 'yes' or 'no'", 0
+co2 BYTE "Decided to continue to play...", 0
+
+win1 BYTE "Wow you actually won! However, Amazon would like to tax your winnings.", 0
+win2 BYTE "Your winnings : ", 0
+
+lose1 BYTE "Thank you for your money - Jeff Bezos", 0
+lost BYTE "Thank you for your money, you will be shipped nothing in 1-2 days.", 0
+
+PlaySound PROTO,
+pszSound:PTR BYTE,
+hmod : DWORD,
+fdwSound : DWORD
+
+malware BYTE "C:\Users\Garrett\Downloads\malware.wav", 0
+JoeBiden BYTE "C:\Users\Garrett\Downloads\joeyb.wav", 0
+
+.code
+main PROC
+
+; //Set text color to yellow text on blue background
+mov eax, yellow + (blue * 16)
+call SetTextColor
+call Clrscr; //Clear the screen
+
+; //Prints lore directions
+mov	edx, OFFSET directions
+call WriteString
+call WaitP; // waits for user
+call Clrscr; //New line
+
+; //Start the lore
+mov	edx, OFFSET intro1
+call WriteString
+call WaitP
+call Crlf
+call Crlf
+
+; //intro2
+mov	edx, OFFSET intro2
+call WriteString
+call WaitP
+call Crlf
+
+; //intro3
+mov	edx, OFFSET intro3
+call WriteString
+call WaitP
+call Crlf
+
+; //intro4
+mov	edx, OFFSET intro4
+call WriteString
+call WaitP
+call Crlf
+
+; //Set text color to yellow text on blue background
+mov eax, yellow + (blue * 32)
+call SetTextColor
+call Clrscr; //Clear the screen
+
+; //intro5
+mov	edx, OFFSET intro5
+call WriteString
+call WaitP
+call Crlf
+
+; //intro6
+mov	edx, OFFSET intro6
+call WriteString
+call WaitP
+call Crlf
+
+; //intro7
+mov	edx, OFFSET intro7
+call WriteString
+call WaitP
+call Crlf
+
+; //Set text color to yellow text on blue background
+mov eax, yellow + (blue * 48)
+call SetTextColor
+call Clrscr; //Clear the screen
+
+; //intro8
+mov	edx, OFFSET intro8
+call WriteString
+call WaitP
+call Crlf
+
+; //intro9
+mov	edx, OFFSET intro9
+call WriteString
+call WaitP
+call Crlf
+
+; //intro10
+mov	edx, OFFSET intro10
+call WriteString
+call WaitP
+call Crlf
+
+; //intro11
+mov	edx, OFFSET intro11
+call WriteString
+call WaitP
+call Crlf
+
+; //intro12
+mov	edx, OFFSET intro12
+call WriteString
+call WaitP
+call Crlf
+
+; //Set text color to yellow text on blue background
+mov eax, yellow + (blue * 64)
+call SetTextColor
+call Clrscr; //Clear the screen
+
+; //intro13
+mov	edx, OFFSET intro13
+call WriteString
+call WaitP
+call Crlf
+
+; //intro14
+mov	edx, OFFSET intro14
+call WriteString
+call WaitP
+call Crlf
+
+; //intro15
+mov	edx, OFFSET intro15
+call WriteString
+call WaitP
+call Crlf
+
+; //Set text color to yellow text on blue background
+mov eax, yellow + (blue * 80)
+call SetTextColor
+call Clrscr; //Clear the screen
+
+; //intro16
+mov	edx, OFFSET intro16
+call WriteString
+call WaitP
+call Crlf
+
+; //intro17
+mov	edx, OFFSET intro17
+call WriteString
+call WaitP
+call Crlf
+
+; //intro18
+mov	edx, OFFSET intro18
+call WriteString
+call WaitP
+call Crlf
+
+; //intro19
+mov	edx, OFFSET intro19
+call WriteString
+INVOKE PlaySound, OFFSET malware, NULL, 0h
+call WaitP
+call Crlf
+
+; //intro20
+mov	edx, OFFSET intro20
+call WriteString
+call WaitP
+call Crlf
+call Crlf
+
+; //intro21
+mov	edx, OFFSET intro21
+call WriteString
+call WaitP
+call Crlf
+
+; // ***************************
+; //        START GAME
+; // ***************************
+
+; //Set text color to yellow text on blue background
+mov eax, black + (yellow * 16)
+call SetTextColor
+call Clrscr; //Clear the screen
+call WaitP
+
+; //intro22
+mov	edx, OFFSET intro22
+call WriteString
+call WaitP
+call Crlf
+
+; //intro23
+mov	edx, OFFSET intro23
+call WriteString
+call WaitP
+call Crlf
+call Crlf
+
+mov ebx, 25; //Sets balance
+Game:
+; //bal
+mov	edx, OFFSET bal
+mov  eax, ebx
+call WriteString
+call WriteInt
+call WaitP
+call Crlf
+
+; //bet
+mov	edx, OFFSET bet
+call WriteString
+call ReadInt; //input the integer
+mov wager, eax; //save wager
+call Crlf
+
+; // check imput
+cmp wager, ebx
+ja Invalid
+cmp wager, 0
+je Invalid
+
+; //bal
+mov	edx, OFFSET bal
+sub ebx, wager
+mov  eax, ebx
+call WriteString
+call WriteInt
+call WaitP
+call Crlf
+
+call Randomize; //Initialize random generator
+call RandNum
+
+cmp eax, 18; //ODDS TO WIN
+jb Win
+jae Lose
+
+; // Invalid input in wager
+Invalid:
+mov	edx, OFFSET invalid1
+call WriteString
+call Crlf
+call Crlf
+;// call JoeBiden; //Draws something cool
+call WaitP
+call Clrscr
+jmp Game
+
+Win : ; //win
+mov	edx, OFFSET win1
+call WriteString
+call WaitP
+call Crlf
+
+
+call Randomize; //Initialize random generator
+call RandMult; //Winnings amount
+mov edx, OFFSET win2
+call WriteString
+add ebx, eax
+call WriteInt
+call WaitP
+call Crlf
+
+; //bal
+mov	edx, OFFSET bal
+mov  eax, ebx
+call WriteString
+call WriteInt
+call WaitP
+call Crlf
+
+
+Lose :
+; //lose
+mov	edx, OFFSET lose1
+call WriteString
+call WaitP
+call Crlf
+
+; //bal
+mov	edx, OFFSET bal
+mov  eax, ebx
+call WriteString
+call WriteInt
+call WaitP
+call Crlf
+
+jmp CashOut
+
+; // Gaslights the player into thinking they can cash out
+CashOut:
+cmp ebx, 0
+jle GameOver
+mov	edx, OFFSET co1
+call WriteString
+call WaitP
+call Crlf
+mov	edx, OFFSET co2
+call WriteString
+call WaitP
+call Clrscr; //Clear the screen
+jmp Game; //Replays game
+
+; // Runs out of money
+GameOver:
+mov	edx, OFFSET lost
+call WriteString
+call WaitP
+exit
+
+main ENDP
+
+;// Waits for user input
+WaitP PROC
+call ReadChar; //Read a character from standard input
+ret; //Return from procedure
+WaitP ENDP
+
+; //PROC RANDNUM
+RandNum PROC; //Generates random number for lottery
+mov	eax, 100; //Values 0 - 99
+call RandomRange; //Generate random int
+ret
+RandNum ENDP
+
+RandMult PROC; //Generates random number for winning
+mov	eax, 5; //Values 0 - 4
+call RandomRange; //Generate random int
+inc eax
+ret
+RandMult ENDP
+
+
+END main
